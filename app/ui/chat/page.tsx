48e581296a7bd1d277c5debd6ac6e4d89d24daaf
Rithy Bondeth <rithybondeth999@gmail.com>
2025-10-24 14:48:11 +0700

    handle chat ui

---
+++ app/ui/chat/page.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { LucideSendHorizonal } from "lucide-react";
+import React, { useState } from "react";
+import { useChat } from "@ai-sdk/react";
+import { Spinner } from "@/components/ui/spinner";
+
+type TUIMessage = {
+  id: string;
+  role: "user" | "assistant";
+  parts: TTextUIPart[];
+};
+
+type TTextUIPart = {
+  type: "text";
+  text: string;
+};
+
+export default function ChatPage() {
+  const [input, setInput] = useState<string>("");
+
+  const { messages, sendMessage, status, error, stop } = useChat();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    sendMessage({ text: input });
+    setInput("");
+  };
+
+  return (
+    <div className="h-screen overflow-y-hidden w-1/2 mx-auto flex flex-col">
+      {/* Display Messages */}
+      <div className="min-h-[90%] w-full py-10 overflow-y-scroll no-scrollbar">
+        {error && <div className="text-red-500">{error.message}</div>}
+        {messages.map((message) => (
+          <div key={message.id}>
+            <div>{message.role === "user" ? "You:" : "Assistant:"}</div>
+            {message.parts.map((part, index) => {
+              switch (part.type) {
+                case "text":
+                  return (
+                    <div
+                      key={`${message.id}-${index}`}
+                      className="whitespace-pre-wrap"
+                    >
+                      {part.text}
+                    </div>
+                  );
+                default:
+                  null;
+              }
+            })}
+          </div>
+        ))}
+        {(status === "submitted" || status === "streaming") && <Spinner />}
+      </div>
+
+      {/* Input Text - Prompt */}
+      <form
+        onSubmit={handleSubmit}
+        className="h-[10%] w-full flex items-center justify-center"
+      >
+        <div className="w-full flex items-center justify-between gap-3 px-1">
+          <Input
+            placeholder="How can i help you?"
+            className="h-12"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          {status === "submitted" || status === "streaming" ? (
+            <Button
+              className="text-xs h-12"
+              variant="destructive"
+              type="button"
+              onClick={stop}
+            >
+              Stop
+            </Button>
+          ) : (
+            <Button
+              className="text-xs h-12"
+              type="submit"
+              disabled={status !== "ready"}
+            >
+              Send
+              <LucideSendHorizonal />
+            </Button>
+          )}
+        </div>
+      </form>
+    </div>
+  );
+}

db1e94112de1c0e953d5e8bdb0ee3e781fcd301c
Rithy Bondeth <rithybondeth999@gmail.com>
2025-10-24 10:27:11 +0700

    updated ubuntu font

--- app/ui/completion/page.tsx
+++ app/ui/completion/page.tsx
@@ -1,9 +1,24 @@
-"use client"
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function CompletionPage() {
-    return (
-        <div>
-            {/* Display Completion Text */}
+  return (
+    <div className="h-screen w-1/2 mx-auto flex flex-col">
+      {/* Display Completion Text */}
+      <div className="h-[90%] w-full"></div>
+
+      {/* Input Text */}
+      <form
+        action=""
+        className="h-[10%] w-full flex items-center justify-center"
+      >
+        <div className="w-full flex items-center justify-between gap-3">
+          <Input placeholder="Enter your message here..." />
+          <Button>Send</Button>
         </div>
-    )
+      </form>
+    </div>
+  );
 }

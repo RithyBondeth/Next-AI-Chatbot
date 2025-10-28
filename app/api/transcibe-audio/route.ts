5defaa8e505ab1404051b73b4466b7e681e9df79
Rithy Bondeth <rithybondeth999@gmail.com>
2025-10-28 11:08:37 +0700

    handle transcribe-audio route

---
+++ app/api/transcibe-audio/route.ts
@@ -0,0 +1,25 @@
+import { openai } from "@ai-sdk/openai";
+import { experimental_transcribe as transcribe } from "ai";
+
+export async function POST(req: Request) {
+  try {
+    const formData = await req.formData();
+    const audioFile = formData.get("audio") as File;
+
+    if (!audioFile)
+      throw new Response("No audio file provided ", { status: 400 });
+
+    const arrayBuffer = await audioFile.arrayBuffer();
+    const unit8Array = new Uint8Array(arrayBuffer);
+
+    const transcript = await transcribe({
+      model: openai.transcription("whisper-1"),
+      audio: unit8Array,
+    });
+
+    return Response.json(transcript);
+  } catch (error) {
+    console.error("Error transcribing audio: ", error);
+    return new Response("Failed to transcribing audio", { status: 500 });
+  }
+}
